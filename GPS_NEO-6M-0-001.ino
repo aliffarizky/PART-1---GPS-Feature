2d92c7582d233c0f1525b54162906532f8965387
aliffarizky <58410206+aliffarizky@users.noreply.github.com>
2019-12-02 10:58:24 +0700

    Add files via upload

---
+++ GPS_NEO-6M-0-001.ino
@@ -0,0 +1,49 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+static const int RXPin = 3, TXPin = 4;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+TinyGPSCustom snr(gps, "GPGSV", 7);
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup()
+{
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+}
+
+void loop()
+{
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+    Serial.println();
+    Serial.print(F("GPS Strength: ")); Serial.print(snr.value()); 
+    
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+  Serial.println();
+}
